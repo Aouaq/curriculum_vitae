423c1a0912f56f89cf3d143729e8f49b886e83e1
Aouaq <128398427+Aouaq@users.noreply.github.com>
2025-11-06 10:23:49 +0100

    Update Flutter SDK version in build script

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
 
-# Install dependencies
-apt-get update && apt-get install -y curl git unzip xz-utils zip libglu1-mesa
-
-# Download Flutter SDK
-curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.24.3-stable.tar.xz
-tar xf flutter_linux_3.24.3-stable.tar.xz
+# Download latest Flutter stable (adjust version!)
+curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.13.6-stable.tar.xz
+tar xf flutter_linux_3.13.6-stable.tar.xz
 export PATH="$PATH:`pwd`/flutter/bin"
 
-# Enable web
+# Fix git dubious ownership issue
+git config --global --add safe.directory /vercel/path0/flutter
+
+# Enable Flutter web
 flutter config --enable-web
 
-# Build the web version
+# Get dependencies and build
 flutter pub get
 flutter build web --release
